ca74ced5b4912571bbb6a8e8e7ee00f67f656174
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 13:38:32 -0800

    saved users controller

--- Controller/userscontrollers.js
+++ Controller/userscontrollers.js
@@ -1,11 +1,34 @@
+const UserModels = require("../Models/usersmodels")
 
+const transporter = require("../Config/mailer")
 
 // Users registration:
 // Users will get a welcome email here on registration
 const NewUsersRegistration = async(req, res) =>{
     try {
         const { name, email, password } = req.body;
-         
+
+        const users = await UserModels.create({
+            name,
+            email,
+            password
+        });
+
+        const welcomeMail = {
+            from: "valerianpedro03@gmail.com",
+            to: email,
+            subject: "Welcome to Sylvia Platform",
+            text: `Dear ${users.name}, Thank you for signing up on our platform, we look forward to having you as a registered user. Please refer us and we will serve you to the best of our capacity`
+        };
+
+        await transporter.sendMail(welcomeMail, function(error, info){
+            if (error) {
+                
+            } else {
+                
+            }
+        })
+
     } catch (error) {
         return res.status(400).json({
             message: "An error occured in registering new users",
