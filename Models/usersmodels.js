dc65cb01dff9838ccb068ddbe50af75b6b68f816
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 10:39:06 -0800

    users models

---
+++ Models/usersmodels.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
