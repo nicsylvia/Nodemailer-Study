80a6ece3c79402bfac741addd2f00ceb907ee3b9
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 12:17:13 -0800

    saved mailer configuration

--- Config/mailer.js
+++ Config/mailer.js
@@ -1,3 +1,11 @@
 const nodemailer = require("nodemailer");
 
-const SendinblueTransporter = require("")
+const SendinblueTransporter = require("nodemailer-sendinblue-transporter");
+
+const environmentVariables = require("./secret");
+
+const transporter = nodemailer.createTransport( new SendinblueTransporter({
+    apikey: environmentVariables.apikey
+}))
+
+module.exports = transporter
